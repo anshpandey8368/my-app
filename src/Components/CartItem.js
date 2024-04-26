29c1810436294fecc8d506a9a26ade90cdbf01b2
Ansh Pandey <anshpandey8368@gmail.com>
2024-04-26 23:47:19 +0530

    ShopcartApp

--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -1,15 +1,12 @@
-
-// import {AiFillDelete} from "react-icons/ai"
 import { useDispatch } from "react-redux";
 import { remove } from "../redux/Slices/CartSlice";
-// import { toast } from "react-hot-toast";
+
 
 const CartItem = ({item, itemIndex}) => {
   const dispatch = useDispatch();
 
   const removeFromCart = () => {
     dispatch(remove(item.id));
-    // toast.success("Item Removed");
   }
 
   return (
